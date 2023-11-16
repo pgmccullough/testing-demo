4ec53439929011e4625ecb2c1d75f37ef2256574
Patrick Glendon McCullough <p@mccullo.ug>
2023-11-16 13:50:07 -0500

    fail from dev

--- src/utils/dateToReadable.test.js
+++ src/utils/dateToReadable.test.js
@@ -1,5 +1,5 @@
 import { dateToReadable } from "./dateToReadable";
 
 it("converts a date from timestamp", () => {
-  expect(dateToReadable(481006800000)).toEqual('March 29, 1985');
+  expect(dateToReadable(481006800000)).toEqual('March 28, 1985');
 })
