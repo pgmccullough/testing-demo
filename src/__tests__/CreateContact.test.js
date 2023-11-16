5bd64202dcd6462651fe66d72b7fa986ec8538c7
Patrick Glendon McCullough <p@mccullo.ug>
2023-11-16 15:27:55 -0500

    horsin' around

--- src/__tests__/CreateContact.test.js
+++ src/__tests__/CreateContact.test.js
@@ -21,48 +21,31 @@ describe('MyForm component', () => {
     expect(posts.length).toBe(10);
   })
 
-  test('Phone number is automatically formatted', () => {
-    render(<NewPostForm />);
-    const phoneInput = screen.getByLabelText('Phone');
-    fireEvent.change(phoneInput, { target: { value: '9172242246' } });
-    expect(phoneInput).toHaveValue('(917) 224-2246');
-  });
+  // test('Adding contact works', () => {
+  //   let fakeState = users;
+  //   const onFormSubmit = (data) => {
+  //     fakeState = [data, ...users];
+  //   };
 
-  test('Error displayed if email is incorrect format', () => {
-    render(<NewPostForm />);
-    const firstNameInput = screen.getByLabelText('First Name');
-    const lastNameInput = screen.getByLabelText('Last Name');
-    const emailInput = screen.getByLabelText('Email');
-    const phoneInput = screen.getByLabelText('Phone');
-    const submitButton = screen.getByText('Add Contact');
+  //   render(<NewPostForm setContacts={onFormSubmit} />);
+  //   const { container } = render(<Posts contacts={fakeState} />);
+    
+  //   const firstNameInput = screen.getByLabelText('First Name');
+  //   const lastNameInput = screen.getByLabelText('Last Name');
+  //   const emailInput = screen.getByLabelText('Email');
+  //   const phoneInput = screen.getByLabelText('Phone');
+  //   const submitButton = screen.getByText('Add Contact');
 
-    fireEvent.change(firstNameInput, { target: { value: 'Patrick' } });
-    fireEvent.change(lastNameInput, { target: { value: 'McCullough' } });
-    fireEvent.change(emailInput, { target: { value: 'pmccullough@' } });
-    fireEvent.change(phoneInput, { target: { value: '(917) 224-2246' } });
+  //   fireEvent.change(firstNameInput, { target: { value: 'Patrick' } });
+  //   fireEvent.change(lastNameInput, { target: { value: 'McCullough' } });
+  //   fireEvent.change(emailInput, { target: { value: 'pmccullough@brainstation.io' } });
+  //   fireEvent.change(phoneInput, { target: { value: '(917) 224-2246' } });
 
-    fireEvent.click(submitButton);
+  //   fireEvent.click(submitButton);
 
-    const emailErrorMessage = screen.getByText('Please enter a valid email address');
-    expect(emailErrorMessage).toBeInTheDocument();
-  });
+  //   const posts = container.querySelectorAll('.post__header');
+  //   expect(posts.length).toBe(11);
+
+  // });
   
-  test('Error displayed if phone is incorrect format', () => {
-    render(<NewPostForm />);
-    const firstNameInput = screen.getByLabelText('First Name');
-    const lastNameInput = screen.getByLabelText('Last Name');
-    const emailInput = screen.getByLabelText('Email');
-    const phoneInput = screen.getByLabelText('Phone');
-    const submitButton = screen.getByText('Add Contact');
-
-    fireEvent.change(firstNameInput, { target: { value: 'Patrick' } });
-    fireEvent.change(lastNameInput, { target: { value: 'McCullough' } });
-    fireEvent.change(emailInput, { target: { value: 'pmccullough@brainstation.io' } });
-    fireEvent.change(phoneInput, { target: { value: '(917) 224-224' } });
-
-    fireEvent.click(submitButton);
-
-    const phoneErrorMessage = screen.getByText('Please enter a valid phone number');
-    expect(phoneErrorMessage).toBeInTheDocument();
-  });
 });
